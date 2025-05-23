1b5f68d2b137af17970c60e79f1165ab18e4a4d1
mohitrajora <mohitrajora1432@gmail.com>
2025-05-24 01:20:22 +0530

    node js

---
+++ Node JS/Introduction/server.js
@@ -0,0 +1,7 @@
+console.log("Server is running...");
+const slugify = require('slugify');
+
+const slug = slugify("Hello World!");
+console.log(slug); // Output: "hello-world"
+
+// it convert text into slug
